ed3b1e69836842be0450482346664465987edcdd
Catalyst IT Services <jburley@catalystitservices.com>
2012-10-23 11:14:54 -0700

    Completed problem 03 for project euler

---
+++ problem03.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+print "Please enter a number: ";
+chomp(my $number = <STDIN>);
+
+my %primeHash;
+my @primeList;
+my $currentLimit = $number;
+
+#http://math.about.com/od/prealgebra/ht/How-To-Find-Out-If-A-Number-Is-Prime-Use-Factorization.htm
+for(my $i = 2; $i <= $currentLimit; $i++) {
+	if($currentLimit % $i == 0) {
+		$currentLimit = $currentLimit / $i;
+		$primeHash{$i} = $currentLimit;
+		$i = 1;
+	}
+}
+print "\n";
+foreach $key (keys %primeHash) {
+	push(@primeList, $key);
+	printf("%10s => $primeHash{$key}\n", "$key");
+}
+print "\n";
+@primeList = sort { $a <=> $b } @primeList;
+print "The primes for ($number) is (@primeList).\n";
+
