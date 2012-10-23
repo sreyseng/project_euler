ca07ea321f1b17a62ef79d07313630a285920d6f
Catalyst IT Services <jburley@catalystitservices.com>
2012-10-23 15:15:40 -0700

    Problem 10

--- problem10.pl
+++ problem10.pl
@@ -1,40 +1,31 @@
 #!/usr/bin/perl
 
-print "Please enter a N(th) position of the prime number you are looking for: ";
-chomp(my $primePosition = <STDIN>);
+print "Please enter a number: ";
+chomp(my $input = <STDIN>);
 
-my @primeList;
-my $primeListSize = 0;
+my @primeList = (2, 3, 5);
+my $sum = 0;
 
-for (my $value = 2; $primeListSize < $primePosition; $value++) {
+for (my $value = 6; $value < $input; $value++) {
 	if(&isPrime($value) == 1) {
 		push(@primeList, $value);
-		$primeListSize = scalar @primeList;
 	}
 }
+
+
+foreach (@primeList) {
+	$sum += $_;
+}
+
+print "$sum\n";
 
 sub isPrime {
-	my %primeHash;
 	my $number = $_[0];
-	my $currentLimit = $number;
 
-	for(my $i = 2; $i <= $currentLimit; $i++) {
-		if($currentLimit % $i == 0) {
-			$currentLimit = $currentLimit / $i;
-			$primeHash{$i} = $currentLimit;
-			$i = 1;
-
-			#if the number is divisible by something, determine if that something is 
-			#the number itself. If not, it is not a prime.
-
-			if(exists $primeHash{$number}) {
-				return 1;
-			} else {
-				return 0;
-			}
+	for(my $i = 0; $i <= sqrt($number); $i++) {
+		if($number % $primeList[$i] == 0) {
+			return 0;
 		}
 	}
+	return 1;	
 }
-
-#print the prime at specified position (offset by 1 for 0-based array)
-print "The prime that is at position $primePosition is $primeList[$primePosition -1].\n"
