44b2a4d26ae79b973482b5f31a037bd12c16a6ab
Catalyst IT Services <jburley@catalystitservices.com>
2012-10-31 09:36:17 -0700

    Added comments to problem 35

--- problem35.pl
+++ problem35.pl
@@ -7,20 +7,21 @@ chomp(my $input = <STDIN>);
 my @primeList = (2, 3, 5);
 my @circularPrimeList;
 
+#Find all the primes
 for (my $value = 6; $value < $input; $value++) {
 	if(&isPrime($value) == 1) {
 		push(@primeList, $value);
 	}
 }
 
-
+#for each prime, check to see if it is a circular prime
 for (my $value = 0; $value < $#primeList + 1; $value++) {
 	if(&checkIsPrimeVariations($primeList[$value]) == 1) {
 		push(@circularPrimeList, $primeList[$value])
 	}
 }
 
-
+#find variations of the prime passed in and check to see if that is prime, else return 0 immediately
 sub checkIsPrimeVariations {
 	my $tempNumber = $_[0];
 	my @tempArray = split(//, $tempNumber);
@@ -36,6 +37,7 @@ sub checkIsPrimeVariations {
 	return 1;
 }
 
+#check if something is prime
 sub isPrime {
 	my $number = $_[0];
 	for(my $i = 0; $i <= sqrt($number); $i++) {
