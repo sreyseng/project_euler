f99e44be9793d7f3e51c1091a17eda3bab6956eb
Catalyst IT Services <jburley@catalystitservices.com>
2012-10-31 09:44:19 -0700

    Remove comments from problem 35, remove creation of circular list, simply total up instead.

--- problem35.pl
+++ problem35.pl
@@ -5,26 +5,23 @@ print "Please enter a number below which you want to find circular primes: ";
 chomp(my $input = <STDIN>);
 
 my @primeList = (2, 3, 5);
-my @circularPrimeList;
-
-#Find all the primes
 for (my $value = 6; $value < $input; $value++) {
 	if(&isPrime($value) == 1) {
 		push(@primeList, $value);
 	}
 }
 
-#for each prime, check to see if it is a circular prime
+my $totalCircularPrime = 0;
 for (my $value = 0; $value < $#primeList + 1; $value++) {
 	if(&checkIsPrimeVariations($primeList[$value]) == 1) {
-		push(@circularPrimeList, $primeList[$value])
+		$totalCircularPrime++;
 	}
 }
 
-#find variations of the prime passed in and check to see if that is prime, else return 0 immediately
+print "There are [$totalCircularPrime] circular primes below [$input].\n";
+
 sub checkIsPrimeVariations {
-	my $tempNumber = $_[0];
-	my @tempArray = split(//, $tempNumber);
+	my @tempArray = split(//, $_[0]);
 	my $length = $#tempArray;
 	for(my $i = 0; $i < $length; $i++) {
 		my $tempShift = shift(@tempArray);
@@ -37,7 +34,6 @@ sub checkIsPrimeVariations {
 	return 1;
 }
 
-#check if something is prime
 sub isPrime {
 	my $number = $_[0];
 	for(my $i = 0; $i <= sqrt($number); $i++) {
@@ -47,8 +43,3 @@ sub isPrime {
 	}
 	return 1;	
 }
-
-print "\n";
-print "The circular primes under $input is: ";
-print $#circularPrimeList + 1;
-print "\n";
