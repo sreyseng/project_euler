d9bc32d57a235b1e5c76302c8abf9a54df0d79a9
Srey Seng <sreyseng@gmail.com>
2013-04-02 10:38:10 -0700

    removed comments (wouldn't run with the comments in...)

--- problem_22/problem22.pl
+++ problem_22/problem22.pl
@@ -4,10 +4,8 @@ use 5.010;
 use strict;
 use warnings;
 use Time::HiRes qw/time/;
-
 my $startTime = time;
 
-#Alphabets and their position scores in a hash
 my %alphabet_hash = (
 "A" => 1,
 "B" => 2,
@@ -35,26 +33,21 @@ my %alphabet_hash = (
 "X" => 24,
 "Y" => 25,
 "Z" => 26
-);
-#read in data file to one big string
-my $data_file = "names.txt";
+);my $data_file = "names.txt";
 open(FILE, $data_file) || die("Could not open file! Ensure that file $data_file exists in working directory...");my $blob = <FILE>;close(FILE);
+$blob =~ s/"//g;
 
-#strip big string of quotes$blob =~ s/"//g;
-
-#split big string into array of names
 my @names_array = split(',', $blob);
 
-#sort the array of names in alphabetical order
-@names_array = sort(@names_array);my $sum = 0;
+@names_array = sort(@names_array);my $sum = 0;
 my $count = 1;
-foreach my $name (@names_array) { #for each name in the array of names
-    my @name_array = split('', $name); # split the name into letters
+foreach my $name (@names_array) {
+    my @name_array = split('', $name);
     
     my $sub_total = 0;
-    foreach my $letter (@name_array) { #for each letter
-        $sub_total = $sub_total + $alphabet_hash{$letter}; # calculate the sub total    }
-    $sum = $sum + ($sub_total * $count); #calculate total sum so far
+    foreach my $letter (@name_array) {
+        $sub_total = $sub_total + $alphabet_hash{$letter};    }
+    $sum = $sum + ($sub_total * $count);
     $count++;}
 my $endTime = time;print "Total name scores: $sum" . "\n";
 print $endTime - $startTime . "\n";
